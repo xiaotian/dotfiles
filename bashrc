a192369b899e11c2aff4a6e7ea4859679e5b4f16
Xiaotian Guo <guoxiaotian@gmail.com>
2015-10-24 18:53:14 -0700

    some python stuff

--- bashrc
+++ bashrc
@@ -10,14 +10,22 @@ if [ -f ~/.localrc ]; then
 fi
 
 if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
+#pyenv initialization
+if which pyenv > /dev/null; then eval "$(pyenv init -)"; fi
+if which pyenv-virtualenv-init > /dev/null; then eval "$(pyenv virtualenv-init -)"; fi
 
 # python stuff
-export PYTHONSTARTUP=~/.pythonrc.py
+export PYTHONSTARTUP=~/.python_startup.py
 
 ### Added by the Heroku Toolbelt
 export PATH="/usr/local/heroku/bin:$PATH"
+
+# set jdk/java version
+# http://stackoverflow.com/questions/21964709/how-to-change-default-java-version
+export JAVA_HOME='/Library/Java/JavaVirtualMachines/jdk1.7.0_79.jdk/Contents/Home'
 
 # stop terminal from handling ctrl-q and ctrl-s
 # we use ctrl-q in emmet.vim as leader key
 stty start undef
 stty stop undef
+
--- python_startup.py
+++ python_startup.py
@@ -3,7 +3,26 @@ import readline
 import rlcompleter
 import atexit
 import os
+from os import path
 import sys
+from datetime import datetime
+from pprint import pprint
+from timeit import timeit
+from collections import namedtuple
+from collections import defaultdict
+
+assert datetime
+assert pprint
+assert sys
+assert timeit
+
+try:
+    from see import see
+except:
+    print('see module not installed')
+else:
+    assert see
+
 
 #  tab completion
 if 'libedit' in readline.__doc__:
@@ -12,11 +31,11 @@ else:
     readline.parse_and_bind("tab: complete")
 
 # history file
-histfile = os.path.join(os.environ['HOME'], '.pythonhistory')
+histfile = path.join(os.environ['HOME'], '.pythonhistory')
 try:
     readline.read_history_file(histfile)
 except IOError:
     pass
 atexit.register(readline.write_history_file, histfile)
-del os, sys, histfile, readline, rlcompleter
-print("^_^ Ready to go!")
+del histfile, readline, rlcompleter
+print("^_^ type away")
